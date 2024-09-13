d79155688c07c3bd3f16e9b658344bb2af28f865
shourya mishra <shourya.desk@gmail.com>
2024-09-13 12:03:37 +0530

    done

--- src/globalComponents/data/majorCities.js
+++ src/globalComponents/data/majorCities.js
@@ -2,7 +2,8 @@ const majorCities = [
     "Mumbai", "Delhi", "Bengaluru", "Hyderabad", "Ahmedabad",
     "Chennai", "Kolkata", "Pune", "Jaipur", "Surat",
     "Lucknow", "Kanpur", "Nagpur", "Indore", "Thane",
-    "Varanasi", "Prayag Raj", "Bidar Karnataka", "Gulburga", "Goa"
+    "Varanasi", "Prayag Raj", "Bidar Karnataka", "Gulburga", "Goa",
+    "Dharwad", "Hubli", "K R Puram", "R R Nagar", "Majestic"
 ];
 
 export default majorCities
