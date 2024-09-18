333517eed58f6a52709309606e65f525c8ebd375
shourya mishra <shourya.desk@gmail.com>
2024-09-18 18:24:06 +0530

    done

--- src/components/FranchiseAddition/FranchiseList.js
+++ src/components/FranchiseAddition/FranchiseList.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom'
 import DriverList from 'components/FranchiseAddition/DriverTable'
 import { useUserContext } from 'globalComponents/AppContext'
 import FranchiseTable from './FranchisesTable'
+import majorCities from 'globalComponents/data/majorCities'
+import { IoClose } from 'react-icons/io5'
 
 const FranchiseList = () => {
   const { franchiseList } = useUserContext()
+
+  const [ filterArea, setFilterArea ] = useState("")
 
   
   return (
@@ -15,11 +19,36 @@ const FranchiseList = () => {
         </p>
 
         <div className='w-full mt-10 h-10 flex items-center justify-between'>
-          <button
-            className='px-16 rounded-full h-full flex items-center justify-center text-[#FF5C00] text-sm bg-[#FFE3CF]'
-          >
-            Filter
-          </button>
+          <div className='h-10 flex items-center gap-4'>
+            {/* <button
+              className='px-16 rounded-full h-full flex items-center justify-center text-[#FF5C00] text-sm bg-[#FFE3CF]'
+            >
+              Filter
+            </button> */}
+            <select
+              value={filterArea}
+              onChange={e => setFilterArea(e.target.value)}
+              className='w-40 rounded-full text-center focus:outline-none h-full flex items-center justify-center text-[#FF5C00] text-sm bg-[#FFE3CF]'
+            >
+              <option value="">Area</option>
+              {majorCities.map(city => (
+                  <option key={city.name} value={city.name}>{city.name}</option>
+              ))}
+            </select>
+
+            {filterArea &&
+            <p
+              className='w-32 relative rounded-full h-full flex items-center justify-center text-[#FF5C00] text-sm bg-transparent border-2 border-[#FFE3CF]'
+            >
+              {filterArea}
+              
+              <span
+                onClick={() => setFilterArea("")}
+                className='absolute -top-1 text-white p-1 -right-1 bg-[#ffb885] rounded-full flex items-center justify-center'>
+                <IoClose />
+              </span>
+            </p>}
+          </div>
 
           <Link
             to={"/franchise-addition"}
@@ -30,7 +59,7 @@ const FranchiseList = () => {
         </div>
         <div className='w-full mt-5'>
           {franchiseList &&
-          <FranchiseTable franchiseList={franchiseList} />}
+          <FranchiseTable franchiseList={franchiseList} area={filterArea && filterArea !== "" && filterArea} />}
         </div>
     </div>
   )
--- src/components/analytics/components/RatingsChartCard.js
+++ src/components/analytics/components/RatingsChartCard.js
@@ -1,24 +1,79 @@
 import React from 'react'
 import deleteImg from "../../../assets/delete/ratingChart.png"
 import "../../../styles/components/analytics/components/RatingsChartCard.css"
+import { useUserContext } from 'globalComponents/AppContext'
+import { useNavigate } from 'react-router-dom'
 
 const RatingsChartCard = () => {
-  return (
-    <div className='rating-card-main-div'>
+    const { driverList, userList, franchiseList } = useUserContext()
 
-        <div className='all-text-div'>
+    const navigate = useNavigate()
+
+    const data = [
+        { title: "Franchises", list: franchiseList, link: "/franchise-list" },
+        { title: "Drivers", list: driverList, link: "/driver-list" },
+        { title: "Users", list: userList, link: "/user-list" },
+    ]
+
+  return (
+    <div className='rating-card-main-div pt-4'>
+
+        <p className='text-left'>
+            Total numbers:
+        </p>
+
+        {data.map((item, index) => (
+        <div className='w-full mt-5 rounded-xl py-2 border-l-2 border-r-2 border-orange-600 shadow-2xl mb-4 backdrop-blur-md bg-opacity-10 flex items-center justify-between px-5'>
+            <p className='text-orange-600 font-semibold '>
+                {item.title}:
+            </p>
+
+            <div className='flex items-center'>
+                <p className=' font-medium text-black pr-3 border-opacity-30 border-r border-gray-500'>
+                    {(item.list && item.list.length > 0)? item.list.length : 0}
+                </p>
+
+                <button
+                    onClick={() => navigate(item.link)}
+                    className='px-3 py-1 w-fit shadow-md bg-[#EBFFE1] text-nowrap text-[#00A510] text-sm ml-2'
+                >
+                    View All
+                </button>
+            </div>
+        </div>))}
+{/* 
+        <div className='w-full rounded-xl py-2 border-l-2 border-r-2 border-orange-600 shadow-2xl mb-4 backdrop-blur-md bg-opacity-10 flex items-center justify-between px-5'>
+            <p className='text-orange-600 font-semibold '>
+                Users:
+            </p>
+
+            <p className=' font-medium text-black pr-3 border-opacity-30 border-r border-gray-500'>
+                {(userList && userList.length > 0)? userList.length : 0}
+            </p>
+        </div>
+
+        <div className='w-full rounded-xl py-2 border-l-2 border-r-2 border-orange-600 shadow-2xl mb-4 backdrop-blur-md bg-opacity-10 flex items-center justify-between px-5'>
+            <p className='text-orange-600 font-semibold '>
+                Franchises:
+            </p>
+
+            <p className=' font-medium text-black pr-3 border-opacity-30 border-r border-gray-500'>
+                {(franchiseList && franchiseList.length > 0)? franchiseList.length : 0}
+            </p>
+        </div> */}
+
+        {/* <div className='all-text-div'>
             <p className='main-title'>
                 Ratings
             </p>
 
             <p className='percentage-text'>
-                {/* <span>Lorem ipsum dolor sit amet, consectetur</span> */}
             </p>
         </div>
 
         <div className='chart-container'>
             <img src={deleteImg} style={{ width: "100%" }} alt='remove' />
-        </div>
+        </div> */}
     </div>
   )
 }
